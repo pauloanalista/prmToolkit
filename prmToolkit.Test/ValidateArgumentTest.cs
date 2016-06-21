338f438052e82ccd673425f07518107a159f95b1
Paulo Rogério Martins Marques <paulo.analista@outlook.com>
2016-06-21 11:41:57 -0300

    Lib de apoio a outros projetos

---
+++ prmToolkit.Test/ValidateArgumentTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using prmToolkit.Validation;
+
+namespace prmToolkit.Test
+{
+    [TestClass]
+    public class ValidateArgumentTest
+    {
+        [TestMethod]
+        public void GetExceptionCollection()
+        {
+            var result = ValidateArgument.GetListException(
+                                    ValidateArgument.IsNotNull(null, "object is required"),
+                                    ValidateArgument.IsEmail("email_invalid", "email invalid")
+
+
+                );
+
+            Assert.IsNotNull(result, "object required");
+            Assert.IsTrue(result.Count == 2, "There should be two exceptions");
+
+        }
+
+
+        [TestMethod]
+        public void ThrowExceptionCollection()
+        {
+
+            try
+            {
+                ValidateArgument.IsOkContinue(
+                                            ValidateArgument.IsNotNull(null, "object is required"),
+                                            ValidateArgument.IsEmail("email_invalid", "email invalid")
+                                            );
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual(ex.Message, "Um ou mais erros.", "There should be two exceptions");
+            }
+        }
+
+        [TestMethod]
+        public void ThrowIndividualException()
+        {
+            try
+            {
+                ValidateArgument.IsNotNull(null, "object is required", true);
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual(ex.Message, "object is required", "is expected value not null");
+            }
+        }
+    }
+}
