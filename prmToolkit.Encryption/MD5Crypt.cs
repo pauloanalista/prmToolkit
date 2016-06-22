01cfb8675f283e3e24dcc36edb4755f7745d8596
Paulo Rogério Martins Marques <paulo.analista@outlook.com>
2016-06-22 10:33:27 -0300

    Criptografia de dados
    Foi criada uma classe responsável por realizar Criptografia de dados

---
+++ prmToolkit.Encryption/MD5Crypt.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace prmToolkit.Encryption
+{
+    public class MD5Crypt
+    {
+        const string hash = "b58c7fae-5a9d-4a4a-8d89-191a6ecbd70d";
+
+        /// <summary>
+        /// Algoritimo usado para criptografar uma mensagem, podendo ser descriptografado pelo método Decrypt
+        /// </summary>
+        /// <param name="message">Mensagem a ser criptografada</param>
+        /// <returns>Retorna mensagem criptografada</returns>
+        public static string Encrypt(string message)
+        {
+            byte[] Results;
+            UTF8Encoding UTF8 = new UTF8Encoding();
+            MD5CryptoServiceProvider HashProvider = new MD5CryptoServiceProvider();
+            byte[] TDESKey = HashProvider.ComputeHash(UTF8.GetBytes(hash));
+            TripleDESCryptoServiceProvider TDESAlgorithm = new TripleDESCryptoServiceProvider();
+            TDESAlgorithm.Key = TDESKey;
+            TDESAlgorithm.Mode = CipherMode.ECB;
+            TDESAlgorithm.Padding = PaddingMode.PKCS7;
+            byte[] DataToEncrypt = UTF8.GetBytes(message);
+            try
+            {
+                ICryptoTransform Encryptor = TDESAlgorithm.CreateEncryptor();
+                Results = Encryptor.TransformFinalBlock(DataToEncrypt, 0, DataToEncrypt.Length);
+            }
+            finally
+            {
+                TDESAlgorithm.Clear();
+                HashProvider.Clear();
+            }
+            return Convert.ToBase64String(Results);
+        }
+
+        /// <summary>
+        /// Descriptografa uma mensagem que foi criptografada pelo método Encrypt
+        /// </summary>
+        /// <param name="message">Mensagem criptografada pelo método Encrypt</param>
+        /// <returns>Retorna a mensagem descriptografada</returns>
+        public static string Decrypt(string message)
+        {
+            byte[] Results;
+            UTF8Encoding UTF8 = new UTF8Encoding();
+            MD5CryptoServiceProvider HashProvider = new MD5CryptoServiceProvider();
+            byte[] TDESKey = HashProvider.ComputeHash(UTF8.GetBytes(hash));
+            TripleDESCryptoServiceProvider TDESAlgorithm = new TripleDESCryptoServiceProvider();
+            TDESAlgorithm.Key = TDESKey;
+            TDESAlgorithm.Mode = CipherMode.ECB;
+            TDESAlgorithm.Padding = PaddingMode.PKCS7;
+            byte[] DataToDecrypt = Convert.FromBase64String(message);
+            try
+            {
+                ICryptoTransform Decryptor = TDESAlgorithm.CreateDecryptor();
+                Results = Decryptor.TransformFinalBlock(DataToDecrypt, 0, DataToDecrypt.Length);
+            }
+            finally
+            {
+                TDESAlgorithm.Clear();
+                HashProvider.Clear();
+            }
+            return UTF8.GetString(Results);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="input">Criptografa uma mensagem sem descriptografia.</param>
+        /// <returns>Retorna mensagem criptografada</returns>
+        public static string EncryptMD5(string input)
+        {
+            MD5 md5 = MD5.Create();
+            byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+            byte[] hash = md5.ComputeHash(inputBytes);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+            return sb.ToString();
+        }
+
+    }
+}
---
+++ prmToolkit.Test/MD5CryptTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using prmToolkit.Encryption;
+
+namespace prmToolkit.Test
+{
+    [TestClass]
+    public class MD5CryptTest
+    {
+        [TestMethod]
+        public void MD5Crypt_Encrypt_Decrypt()
+        {
+            string senha = "Paulo Rogério";
+
+            string senhaCriptografada = MD5Crypt.Encrypt(senha);
+
+            string senhaDesCriptografada = MD5Crypt.Decrypt(senhaCriptografada);
+
+
+            string senhaMD5 = MD5Crypt.EncryptMD5(senha);
+
+            Assert.AreEqual(senhaCriptografada, "sE0hI8fXaecNxoHI2IokuQ==");
+            Assert.AreEqual(senhaDesCriptografada, senha);
+            Assert.AreEqual(senhaMD5, "69103C8FA326DB8B39CB87A6492390C6");
+        }
+    }
+}
