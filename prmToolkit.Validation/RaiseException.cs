00c5be89a7f9d05be170fb54af954c03ba1bf20b
Paulo Rogério Martins Marques <paulo.analista@outlook.com>
2016-07-25 16:25:55 -0300

    Criei 2 métodos novos para validação
    Foram adicionados os métodos IfNotCpf e IfNotCnpj.
    
    Sua finalidade é leventar uma exceção caso o Cpf ou Cnpj passado seja inválido.

--- prmToolkit.Validation/RaiseException.cs
+++ prmToolkit.Validation/RaiseException.cs
@@ -434,5 +434,121 @@ namespace prmToolkit.Validation
 
             return null;
         }
+        
+        /// <summary>
+        /// Se não for um CPF válido, levanta exceção
+        /// </summary>
+        public static Exception IfNotCpf(string cpf, string message, bool generateIndividualException = false)
+        {
+            int[] multiplicador1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            string tempCpf;
+            string digito;
+            int soma;
+            int resto;
+            cpf = cpf.Trim();
+            cpf = cpf.Replace(".", "").Replace("-", "");
+            if (cpf.Length != 11)
+            {
+                if (generateIndividualException == true)
+                {
+                    throw new InvalidOperationException(message);
+                }
+
+                return new InvalidOperationException(message);
+            }
+
+            tempCpf = cpf.Substring(0, 9);
+            soma = 0;
+
+            for (int i = 0; i < 9; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador1[i];
+            resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = resto.ToString();
+            tempCpf = tempCpf + digito;
+            soma = 0;
+            for (int i = 0; i < 10; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador2[i];
+            resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = digito + resto.ToString();
+            bool isValid = cpf.EndsWith(digito);
+
+            if (isValid == false)
+            {
+                if (generateIndividualException == true)
+                {
+                    throw new InvalidOperationException(message);
+                }
+
+                return new InvalidOperationException(message);
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Se não for um Cnpj válido levanta uma exceção
+        /// </summary>
+        public static Exception IfNotCnpj(string cnpj, string message, bool generateIndividualException = false)
+        {
+            int[] multiplicador1 = new int[12] { 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = new int[13] { 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int soma;
+            int resto;
+            string digito;
+            string tempCnpj;
+            cnpj = cnpj.Trim();
+            cnpj = cnpj.Replace(".", "").Replace("-", "").Replace("/", "");
+            if (cnpj.Length != 14)
+            {
+                if (generateIndividualException == true)
+                {
+                    throw new InvalidOperationException(message);
+                }
+
+                return new InvalidOperationException(message);
+            }
+            tempCnpj = cnpj.Substring(0, 12);
+            soma = 0;
+            for (int i = 0; i < 12; i++)
+                soma += int.Parse(tempCnpj[i].ToString()) * multiplicador1[i];
+            resto = (soma % 11);
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = resto.ToString();
+            tempCnpj = tempCnpj + digito;
+            soma = 0;
+            for (int i = 0; i < 13; i++)
+                soma += int.Parse(tempCnpj[i].ToString()) * multiplicador2[i];
+            resto = (soma % 11);
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = digito + resto.ToString();
+            bool isValid = cnpj.EndsWith(digito);
+
+            if(isValid == false)
+            {
+                if (generateIndividualException == true)
+                {
+                    throw new InvalidOperationException(message);
+                }
+
+                return new InvalidOperationException(message);
+            }
+
+            return null;
+        }
     }
 }
