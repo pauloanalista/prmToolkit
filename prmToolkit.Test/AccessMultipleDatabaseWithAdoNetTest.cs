f532ccbe817fd8dde8c4cb7a156f0222f7a4d208
Paulo Rogério Martins Marques <paulo.analista@outlook.com>
2017-03-16 13:17:38 -0300

    Adicionei comentários

--- prmToolkit.Test/AccessMultipleDatabaseWithAdoNetTest.cs
+++ prmToolkit.Test/AccessMultipleDatabaseWithAdoNetTest.cs
@@ -1,39 +1,31 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using prmToolkit.AccessMultipleDatabaseWithAdoNet;
 using prmToolkit.AccessMultipleDatabaseWithAdoNet.Enumerators;
-using prmToolkit.AccessMultipleDatabaseWithAdoNet.Databases;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 namespace prmToolkit.Test
 {
 
     [TestClass]
-    public class AccessMultipleDatabaseWithAdoNetTest : AbstractRepository
+    public class AccessMultipleDatabaseWithAdoNetTest : AbstractRepository //herrda
     {
         [TestMethod()]
         public void ObterDadosTest()
         {
-            Stopwatch stopwatch = new Stopwatch();
-            stopwatch.Start();
-
-            Debug.WriteLine("Início: " + stopwatch.Elapsed.TotalMilliseconds);
-
-            string query = @"select u.nome, u.login, u.senha
-                    from usuario u;
-                    ";
-
-            CommandSql cmd = new CommandSql("Server=seu ip; Database=nome_do_banco; Port=3306; Uid=usuario; Pwd=senha;",
-                                            query, EnumDatabaseType.MySql);
-
+            //Define a string de conexão
+            string  stringConexao = "Server=seu ip; Database=nome_do_banco; Port=3306; Uid=usuario; Pwd=senha;"
             
+            //Monta a query
+            string query = @"select u.nome, u.login, u.senha from usuario u;";
+            
+            //Define em que banco será executada a aquery
+            CommandSql cmd = new CommandSql(stringConexao, query, EnumDatabaseType.MySql);
 
-            List<Usuario> permissoes = GetCollection<Usuario>(cmd)?.ToList();
+            //Obtem os dados do banco de dados MySql
+            List<Usuario> usuarios = GetCollection<Usuario>(cmd)?.ToList();
 
-            stopwatch.Stop();
-            Debug.WriteLine("Fim: " + stopwatch.Elapsed.TotalMilliseconds);
 
-            Assert.IsTrue(permissoes.Count() > 0);
+            Assert.IsTrue(usuarios.Count() > 0);
         }
     }
 
